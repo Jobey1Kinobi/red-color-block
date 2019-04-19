c64af7f2a30b4f05463b3c375b0d6413bcb9b855
Jobey1Kinobi <jobers92@gmail.com>
2019-04-18 21:34:15 -0500

    update

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 $(document).ready(function() {
     $('#redBtn').on('click', whenIclickTheRedButton);
+    $('#blueBtn').on('click', whenIclickTheBlueButton);
+    $('#greenBtn').on('click', whenIclickTheGreenButton);
+    $('#yellowBtn').on('click', WhenIclickTheYellowButton);
 });
 
 let redCount = 0;
-
 function whenIclickTheRedButton() {
     redCount++;
     $('#redCount').text('Red Count: ' + redCount);
     $('#container').append('<div class="box red"></div>');
 }
 
-console.log('Im running outside the document ready!');
 
 
-/*
-function onReady() {
-    console.log('Code to run when it loads!');
+let blueCount = 0;
+function whenIclickTheBlueButton() {
+    blueCount++;
+    $('#blueCount').text('Blue Count: ' + blueCount);
+    $('#container').append('<div class="boxer blue"></div>');
 }
 
-let someFunction = function() {
-    console.log('Hey, Im a function!');
+
+
+let greenCount = 0;
+function whenIclickTheGreenButton() {
+    greenCount++;
+    $('#greenCount').text('Green Count: ' + greenCount);
+    $('#container').append('<div class="boxes green"></div>');
 }
-*/
+
+
+
+let yellowCount = 0;
+function WhenIclickTheYellowButton() {
+    yellowCount++;
+    $('#yellowCount').text('Yellow Count: ' + yellowCount);
+    $('#container').append('<div class="boxed yellow"></div>');
+}
+
+console.log(ColorBoxRocks);
